19c9ff6e5a40be81a0dcb1a4a41d82e28ca80fb9
annachk <annachk@terpmail.umd.edu>
2021-03-02 23:21:15 -0300

    updated script.js and index.html
    Now you can search and get back the matching restaurants

--- public/script.js
+++ public/script.js
@@ -1,3 +1,8 @@
+// The filter is by category
+
+const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'
+x = []
+
 async function windowActions(){
     console.log('Window loaded')
     const search = document.querySelector('#search');
@@ -5,18 +10,47 @@ async function windowActions(){
 
     form.addEventListener('submit', async(event) =>{
         event.preventDefault();
-        const data = await fetch('/api', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({search: search.value})
+        fetch(endpoint)
+            .then(blob => blob.json())
+            .then(data => x.push(...data))
+            
         
         });
-    });
+
     search.addEventListener('input', async(event) => {
         console.log('input', event.target.value);
     });
+    
 }
 
 window.onload = windowActions;
+
+function findMatches(wordToMatch, x){
+    return x.filter(place => {
+
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.category.match(regex)
+    });
+}
+function displayMatches(){
+    const matchArray = findMatches(this.value, x);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const categoryName = place.category.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+            <li>
+                <span class="name">${place.name}</span>
+                <span class="name">${categoryName}</span>
+                <span class="name">${place.address_line_1}</span>
+                <span class="name">${place.city}</span>
+                <span class="name">${place.zip}</span>
+            </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const suggestions = document.querySelector('.suggestions');
+
+search.addEventListener('change', displayMatches);
+search.addEventListener('keyup', displayMatches);
